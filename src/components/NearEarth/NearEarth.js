9bea9854c1d778f9a26ef9e8b3a424e8c5f804cb
Irene R <irene@smallwondermedia.com>
2019-05-10 19:09:08 -0400

    neo data in component

--- src/components/NearEarth/NearEarth.js
+++ src/components/NearEarth/NearEarth.js
@@ -4,6 +4,26 @@ const NearEarth = (props) => {
     return (
         <div className="nearEarthInfo">
 
+            <h3>Name: {props.name}</h3>
+            <br/>
+            Absolute magnitude: {props.magnitude}
+            <br/>
+            Hazardous: {props.hazardous}
+            <br/>
+            JPL URL: {props.jplUrl}
+            <br/>
+            Estimated diameter (max): {props.diameterMilesMax} miles
+            <br/>
+            Estimated diameter (min): {props.diameterMilesMin} miles 
+            <br/>
+            Estimated diameter (max): {props.diameterKiloMax} kilometers
+            <br/>
+            Estimated diameter (min): {props.diameterKiloMin} kilometers
+            <br/>
+            First observed: {props.firstDate}
+            <br/>
+            Last observed: {props.lastDate}
+
 
 
         </div>
