11450a4685ffd37ce328237d1d1cd1f64b22fc21
Irene R <irene@smallwondermedia.com>
2019-05-09 20:31:50 -0400

    returning data in console

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
-    const photoDay = useState([]);
+    const [photoDay, setPhotoDay] = useState([]);
 
-    
+    useEffect(() => {
+        // this code returns data
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API}`)
+        .then(res => {
+            const result = res.data;
+            console.log(result);
+        });
+    }, []);
+
+    // useEffect(() => {
+    //     const result = axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API}`);
+    //     setPhotoDay(result.data);
+    //     console.log(result.data);
+    // }, []);
 
 
   return (
     <div className="App">
 
-
+        {/* <div>
+            { photoDay }
+        </div> */}
 
 
     </div>
