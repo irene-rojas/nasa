d15cef13fac7883ca61b21a35c4e6fa0cc4b36ee
Irene R <irene@smallwondermedia.com>
2019-05-11 00:09:06 -0400

    desktop

--- src/components/PhotoDay/PhotoDay.js
+++ src/components/PhotoDay/PhotoDay.js
@@ -18,7 +18,7 @@ const PhotoDay = (props) => {
             {props.explanation}
             <br/>
             <br/>
-            <a href="https://apod.nasa.gov/apod/astropix.html">Office NASA APOD Page</a>
+            <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank" rel="noopener noreferrer">Office NASA APOD Page</a>
             <br/>
             <a href="https://apod.nasa.gov/apod/archivepix.html" target="_blank" rel="noopener noreferrer">Archive</a>
         </div>
