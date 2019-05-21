6138ed6146b5c7a77f873e7467a8882430172a5c
Irene R <irene@smallwondermedia.com>
2019-05-21 19:15:25 -0400

    disabled ImageSearch component for testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import axios from 'axios';
 import PhotoDay from "./components/PhotoDay/PhotoDay";
 import NearEarth from "./components/NearEarth/NearEarth";
-import ImageSearch from "./components/ImageSearch/ImageSearch";
+// import ImageSearch from "./components/ImageSearch/ImageSearch";
 
 function App() {
 
