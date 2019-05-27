40e3ae7831b28d436c52c7d5a2a13160b73b84d1
Irene R <irene@smallwondermedia.com>
2019-05-26 23:04:34 -0400

    function attempt to find maxSol

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PhotoDayVideo from "./components/PhotoDayVideo/PhotoDayVideo";
 import NearEarth from "./components/NearEarth/NearEarth";
 import ImageSearch from "./components/ImageSearch/ImageSearch";
 import Mars from "./components/Mars/Mars";
+// import MaxSol from "./components/MaxSol/MaxSol";
 
 function App() {
 
@@ -28,11 +29,11 @@ function App() {
     const [marsPhotos, setMarsPhotos] = useState([]);
     const [rover, setRover] = useState("");
     const [sol, setSol] = useState("");
-    // const [maxSol, setMaxSol] = useState([
-    //     {
-    //         "maxSol": ""
-    //     }
-    // ]);
+    const [maxSol, setMaxSol] = useState([
+        {
+            "maxSol": ""
+        }
+    ]);
     const [camera, setCamera] = useState("");
     const [marsError, setMarsError] = useState(false);
 
@@ -88,13 +89,23 @@ function App() {
             setMarsPhotos(res.data.photos.slice(0,24));
             console.log(res.data.photos.slice(0,24));
             console.log(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=${process.env.REACT_APP_NASA_API}`);
-            // console.log(res.data.photos[0].rover.max_sol);
+            console.log(res.data.photos[0].rover.max_sol);
+            setMaxSol(findMaxSol(res.data.photos.slice(0,2)));
             if (!marsPhotos.length > 0) {
                 setMarsError(true);
             }
         });
     };
 
+    function findMaxSol(props) {
+        props.map(prop => 
+            (
+                {
+                    "maxSol": prop.rover.max_sol
+                }
+            )
+        )
+    }
 
   return (
     <div className="App" id="top">
@@ -285,6 +296,7 @@ function App() {
                     {/* need to display max_sol */}
                     <div>
                         Enter sol (Mars mission date) between 0 and XXX:
+
                         <input 
                             type="text"
                             onChange={event => {
---
+++ src/components/MaxSol/MaxSol.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MaxSol = (props) => {
+    return (
+        <span>{props.maxSol}</span>
+    )
+}
+
+export default MaxSol;
