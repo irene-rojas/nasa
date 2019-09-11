41526a0e2f901a5b5b6594c4e9aa024fd9c1ff42
Irene Rojas <irene@smallwondermedia.com>
2019-09-10 21:13:07 -0400

    Updates

---
+++ precache-manifest.8978b9eb0715dd4f3531e8e65ea20b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb73c12cad2bfe89b95c0d00f88a854",
+    "url": "/nasa/index.html"
+  },
+  {
+    "revision": "eccf6082c6de9de889e5",
+    "url": "/nasa/static/css/main.232c8acb.chunk.css"
+  },
+  {
+    "revision": "acfd9baf02d489002480",
+    "url": "/nasa/static/js/2.56e10c69.chunk.js"
+  },
+  {
+    "revision": "eccf6082c6de9de889e5",
+    "url": "/nasa/static/js/main.00872ce2.chunk.js"
+  },
+  {
+    "revision": "a7313f2709f38104e92e",
+    "url": "/nasa/static/js/runtime~main.0a7c813a.js"
+  },
+  {
+    "revision": "b44f0cfed18c22a1ff96db937702a389",
+    "url": "/nasa/static/media/nasa_logo.b44f0cfe.png"
+  },
+  {
+    "revision": "bea2d77de2f1af5a98cc4d7c3b07f0c5",
+    "url": "/nasa/static/media/spinning.bea2d77d.gif"
+  }
+]);
